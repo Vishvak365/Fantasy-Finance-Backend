82fa3f86ceb00075ebcaeaa38e635285acfae642
Vishvak Seenichamy <vishvak.seenichamy-venkatesan.vaauqj@statefarm.com>
2021-08-06 11:17:28 -0400

    Setup CORS

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const dotenv = require("dotenv");
 const firebase = require("./Firebase");
 const account = require("./Routes/account");
 const trade = require("./Routes/trade");
+var cors = require("cors");
 dotenv.config();
+
+app.use(cors());
 
 app.use("/account", account);
 app.use("/trade", trade);
