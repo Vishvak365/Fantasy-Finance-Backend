b3a81d3529b13259c45285ff694026c256919c05
Vishvak Seenichamy <vishvak365@gmail.com>
2021-10-01 14:57:57 -0400

    Setup Base Curr Price

--- Routes/leagues_trade/buy_stock.js
+++ Routes/leagues_trade/buy_stock.js
@@ -2,15 +2,14 @@ var express = require("express");
 var router = express.Router();
 const IEXClient = require("../../IEXClient");
 const firebase = require("../../Firebase");
-
+const getCurrPrice = require("./curr_price");
 const leagues = firebase.firestore().collection("leagues");
 
 async function buy_stock(req, res) {
   const uid = res.locals.uid;
   const body = req.body;
-  const data = await IEXClient.get("/tops?symbols=snap,tsla");
-
-  console.log(data);
+  const currStockPrice = getCurrPrice();
+  
   //Checks to make sure that all the information is provided
   if ((!body.stockName, !body.quantity, !body.leagueId)) {
     res.status(400);
