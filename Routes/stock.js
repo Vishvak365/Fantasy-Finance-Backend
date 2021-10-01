c4965fd9d486f4c3c14241804e692e5cc3ddf2f2
Vishvak Seenichamy <vishvak365@gmail.com>
2021-10-01 15:19:21 -0400

    Add curr_stock_price endpoint

--- Routes/stock.js
+++ Routes/stock.js
@@ -1,16 +1,30 @@
 var express = require("express");
 var router = express.Router();
-var yahooFinance = require('yahoo-finance');
-
+var yahooFinance = require("yahoo-finance");
+const getCurrPrice = require("./leagues_trade/curr_price");
 router.get("/", function (req, res) {
   const uid = res.locals.uid;
-  yahooFinance.quote({
-    symbol: 'GOOG',
-    modules: ['price'] // see the docs for the full list
-  }, function (err, quotes) {
-    console.log(quotes.price['regularMarketPrice'])
-  });
-  res.send("")
+  yahooFinance.quote(
+    {
+      symbol: "GOOG",
+      modules: ["price"], // see the docs for the full list
+    },
+    function (err, quotes) {
+      console.log(quotes.price["regularMarketPrice"]);
+    }
+  );
+  res.send("");
 });
-
+router.get("/currentPrice", async function (req, res) {
+  const stockName = req.query.stockName;
+  if (!stockName) {
+    res.status(300);
+    res.send({ message: "include 'stockName' query param" });
+    return;
+  }
+  const stockPrice = await getCurrPrice(stockName);
+  console.log(stockPrice);
+  res.status(200);
+  res.send({ price: stockPrice });
+});
 module.exports = router;
