270254ec3cbd10013f2de3195584a3484a04d366
Vishvak Seenichamy <vishvak365@gmail.com>
2021-10-01 15:20:16 -0400

    Remove yahoo finance dependencies and endpoints

--- Routes/stock.js
+++ Routes/stock.js
@@ -1,20 +1,6 @@
 var express = require("express");
 var router = express.Router();
-var yahooFinance = require("yahoo-finance");
 const getCurrPrice = require("./leagues_trade/curr_price");
-router.get("/", function (req, res) {
-  const uid = res.locals.uid;
-  yahooFinance.quote(
-    {
-      symbol: "GOOG",
-      modules: ["price"], // see the docs for the full list
-    },
-    function (err, quotes) {
-      console.log(quotes.price["regularMarketPrice"]);
-    }
-  );
-  res.send("");
-});
 router.get("/currentPrice", async function (req, res) {
   const stockName = req.query.stockName;
   if (!stockName) {
