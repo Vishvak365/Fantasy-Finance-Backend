eeeaeee10423cffcf14b1fb26215ddf371addc91
Vishvak Seenichamy <vishvak365@gmail.com>
2021-10-01 15:11:32 -0400

    Finished getting curr stock price information

--- Routes/leagues_trade/curr_price.js
+++ Routes/leagues_trade/curr_price.js
@@ -1,7 +1,22 @@
 const IEXClient = require("../../IEXClient");
 
-async function getCurrPrice() {
-  const data = await IEXClient.get("/tops?symbols=snap,tsla");
-  return 1;
+
+/**
+ * 
+ * @param {*} stockName Stock ticker name of stock
+ * @returns Curr stock price
+ * @example const currStockPrice = await getCurrPrice('tsla');
+ */
+async function getCurrPrice(stockName) {
+  if (!stockName) {
+    return undefined;
+  }
+  try {
+    const data = await IEXClient.get(`/tops?symbols=${stockName}`);
+    return data.data[0].lastSalePrice;
+  } catch (exception) {
+    console.log("Unable to get stock price information from IEX", exception);
+    return undefined;
+  }
 }
 module.exports = getCurrPrice;
